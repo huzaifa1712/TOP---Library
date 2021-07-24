b435cefbec651572f23ea68cc4f0939b65fd46d4
Huzaifa Raghav <windowpane1712@gmail.com>
2021-07-24 12:30:21 +0800

    Grid styling

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-class Book{
-    constructor(title, author, pages, read){
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+// class Book{
+//     constructor(title, author, pages, read){
+//         this.title = title;
+//         this.author = author;
+//         this.pages = pages;
+//         this.read = read;
+//     }
 
-    getDescription(){
-        return `${this.title} by ${author}. ${pages} pages, ${read ? "read." : "Not read yet."}`;
-    }
+//     getReadDesc(){
+//         return this.read ? "Read." : "Not read yet."
+//     }
+
+//     getDescription(){
+//         return `${this.title} by ${author}. ${pages} pages. ${this.getReadDesc()}`;
+//     }
+// }
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.getReadDesc = function(){
+    return this.read ? "Read." : "Not read yet."
+}
+
+Book.prototype.getDescription = function(){
+    return `${this.title} by ${this.author}. ${this.pages} pages. ${this.getReadDesc()}`;
 }
 
 const books = [
@@ -17,5 +36,27 @@ const books = [
     new Book("Man's Search For Meaning", "Viktor Frankl", 200, false),
     new Book("The Three-Body Problem", "Liu Cixin", 302, true),
     new Book("1984", "George Orwell", 328, true),
-    new Book("Animal Farm", "George Orwell", 112, true)
+    new Book("Animal Farm", "George Orwell", 112, true),
+    new Book("A Brief History Of Time", "Stephen Hawking", 256, false)
 ]
+
+const library = document.getElementById("library");
+
+function bookToCard(book){
+    return `<div class = "card">
+            <h3>${book.title}</h3>
+            <p class = "author">${book.author}</p>
+            <p class = "pages">${book.pages} pages</p>
+            <p class = "read">${book.getReadDesc()}</p>
+        </div>`
+}
+
+//  const firstCard = bookToCard(books[0]);
+
+// library.innerHTML += firstCard;
+// library.innerHTML += firstCard;
+// library.innerHTML += firstCard;
+// library.innerHTML += firstCard;
+// library.innerHTML += firstCard;
+
+books.forEach(book => library.innerHTML += bookToCard(book)); 
